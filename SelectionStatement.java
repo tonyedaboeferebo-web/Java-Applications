e5cc1a18ff4297f1de28978e96e68abc44ae144c
tonyedaboeferebo <tonyedaboeferebo@gmail.com>
2025-10-29 12:01:37 +0100

    Initial

---
+++ SelectionStatement.java
@@ -0,0 +1,23 @@
+public class SelectionStatement{
+    public static void main(String[] args){
+		int age = 18;
+		
+		//single selection statement
+		if (age >= 18){
+		    System.out.println("Wow, you are an adult");
+			System.out.println("Being an adult is fun");
+		}
+        
+        //Double selection statement
+		int num1 = 5;
+		int num2 = 25;
+		
+		if(num1 > num2)
+			System.out.printf("%d is greater than %d%n",num1,num2);
+		
+		else
+			System.out.printf("%d is greater than %d%n",num2,num1);
+    }
+	
+	
+}		
